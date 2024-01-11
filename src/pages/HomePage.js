2c736e4c0c727c30167b0a75a82a621fb2d956e2
pangil5634 <oksk6681@naver.com>
2024-01-11 16:58:43 +0900

    24011 MUI / 스타일드 컴포넌트 설치

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,24 @@
-import { Link } from "react-router-dom";
+import {Button} from "@mui/material";
+import {Link} from "react-router-dom";
+import styled from "styled-components";
 
 export default function HomePage() {
     return (
-        <>
+        <div>
             <h1>HomePage</h1>
-            <Link to = "/Test"><button>Test 이동하기</button></Link>
-        </>
+            <Link to="/Test">
+                <DefaultButton>Test 이동하기</DefaultButton>
+            </Link>
+        </div>
     );
 }
+
+const DefaultButton = styled(Button)`
+    background-color: #5CD1E5;
+    color : white;
+            
+    &:hover {
+        color : black;
+        background-color: #B2EBF4;
+    }
+`;
